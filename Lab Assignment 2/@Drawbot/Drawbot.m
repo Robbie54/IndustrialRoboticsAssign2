ec974761f45410211ced02bb6e6cd16904784df3
zum78 <maxtmc@outlook.com>
2023-10-23 20:15:12 +1100

    Drawbot Now Has A Skin

--- Lab Assignment 2/@Drawbot/Drawbot.m
+++ Lab Assignment 2/@Drawbot/Drawbot.m
@@ -12,7 +12,7 @@ classdef Drawbot < RobotBaseClass
 
     methods
         %% Constructor
-        function self = Drawbot(baseTr)%,useTool,toolFilename)
+        function self = Drawbot(baseTr,useTool,toolFilename)
             if nargin < 3
                 if nargin == 2
                     error('If you set useTool you must pass in the toolFilename as well');
@@ -20,30 +20,32 @@ classdef Drawbot < RobotBaseClass
                     baseTr = transl(-0.5,0,0.75);
                 end
             else % All passed in
-                % self.useTool = useTool;
-                % toolTrData = load([toolFilename,'.mat']);
-                % self.toolTr = toolTrData.tool;
-                % self.toolFilename = [toolFilename,'.ply'];
+                self.useTool = useTool;
+                toolTrData = load([toolFilename,'.mat']);
+                self.toolTr = toolTrData.tool;
+                self.toolFilename = [toolFilename,'.ply'];
             end
 
             self.CreateModel();
             self.model.base = self.model.base.T * baseTr;
-            % self.model.tool = self.toolTr;
-            % self.PlotAndColourRobot();
-            q = zeros(4);
-            self.model.plot(q)
+            self.model.tool = self.toolTr;
+            self.PlotAndColourRobot();
+            % q = zeros(4);
+            % self.model.plot(q)
 
             drawnow
         end
 
         %% CreateModel
         function CreateModel(self)
-            link(1) = Link('d',0,'a',0,'alpha',0,'offset',0,'qlim', deg2rad([-90,90]));
-            link(2) = Link('d',0,'a',0.3,'alpha',0,'offset',-3*pi/8,'qlim', deg2rad([-90,90]));
-            link(3) = Link('d',0,'a',0.3,'alpha',0,'offset',7*pi/8,'qlim', deg2rad([-180,180]));
-            link(4) = Link('d',0,'a',0,'alpha',0,'offset',0);
+            % link(1) = Link('d',0,'a',0,'alpha',0,'offset',0,'qlim', deg2rad([-90,90]));
+            link(1) = Link('d',0,'a',0.3,'alpha',0,'offset',-3*pi/8,'qlim', deg2rad([-90,90]));
+            link(2) = Link('d',0,'a',0.3,'alpha',0,'offset',7*pi/8,'qlim', deg2rad([-180,180]));
+            link(3) = Link('d',0,'a',0,'alpha',0,'offset',0);
 
             self.model = SerialLink(link,'name',self.name);
+
+            
         end
     end
 end
